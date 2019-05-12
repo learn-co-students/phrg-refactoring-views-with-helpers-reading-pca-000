246a3c4a6c10228f09ec21b64ea1f25d43932589
Garett Arrowood <garettarrowood@gmail.com>
2019-05-12 06:39:02 -0400

    Add lint test & cleanup repo

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,19 @@
-class Post < ActiveRecord::Base
+# frozen_string_literal: true
 
-  validate :is_title_case
+class Post < ActiveRecord::Base
+  validate :title_case
   before_validation :make_title_case
   belongs_to :author
 
-  private
+private
 
-  def is_title_case
-    if title.split.any?{|w|w[0].upcase != w[0]}
-      errors.add(:title, "Title must be in title case")
-    end
+  def title_case
+    return unless title.split.any? { |w| w[0].upcase != w[0] }
+
+    errors.add(:title, "Title must be in title case")
   end
 
   def make_title_case
-    self.title = self.title.titlecase
+    self.title = title.titlecase
   end
 end
